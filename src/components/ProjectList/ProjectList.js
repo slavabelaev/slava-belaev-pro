c1248bf835885ec32d9a9c2a3f975a2dc5488bbe
Slava Belaev <slav@belaev.pro>
2019-10-27 18:25:27 +0200

    Fix card size on other screen sizes

--- src/components/ProjectList/ProjectList.js
+++ src/components/ProjectList/ProjectList.js
@@ -4,27 +4,40 @@ import withWidth from '@material-ui/core/withWidth';
 import GridList from '@material-ui/core/GridList';
 import Dialog from '@material-ui/core/Dialog';
 import GridListTile from '@material-ui/core/GridListTile';
-import { withStyles } from '@material-ui/core/styles';
+import { withStyles, createStyles } from '@material-ui/core/styles';
 import ProjectItem from '../ProjectItem';
 
-const styles = {
+const styles = createStyles({
     gridList: {
         padding: '.5rem'
     },
     gridListTile: {
-        padding: '.5rem!important'
+        padding: '.5rem!important',
+        width: '100%',
+        '@media (min-width: 600px)': { maxWidth: '50%' },
+        '@media (min-width: 840px)': { maxWidth: '33.3333%' },
+        '@media (min-width: 1280px)': { maxWidth: '25%' },
     }
-};
+});
 
 const ProjectList = ({ classes, projects = [], width }) => {
     const [open, setOpen] = React.useState(false);
     const [dialogHomepage, setDialogHomepage] = React.useState(false);
+    const dialog = (
+        <Dialog open={open} onClose={() => setOpen(false)} fullWidth={true} maxWidth={'xl'}>
+            <iframe
+                src={`https://material.io/resources/resizer/#url=${dialogHomepage}`}
+                frameBorder={0}
+                style={{width: '100%', height: '95vh'}}
+            />
+        </Dialog>
+    );
     return (
         <div>
             <GridList
                 cellHeight={'auto'}
-                cols={{'xs': 1, 'sm': 2, 'md': 3, 'lg': 4, 'xl': 4}[width]}
                 spacing={0}
+                cols={{'xs': 1, 'sm': 2, 'md': 3, 'lg': 4, 'xl': 4}[width]}
                 className={classes.gridList}>
                 {projects.map(project => !project.homepage ? null : (
                     <GridListTile key={project.id} className={classes.gridListTile}>
@@ -36,13 +49,7 @@ const ProjectList = ({ classes, projects = [], width }) => {
                     </GridListTile>
                 ))}
             </GridList>
-            <Dialog open={open} onClose={() => setOpen(false)} fullWidth={true} maxWidth={'xl'}>
-                <iframe 
-                    src={'https://material.io/resources/resizer/#url=' + dialogHomepage} 
-                    frameBorder={0} 
-                    style={{width: '100%', height: '95vh'}} 
-                />
-            </Dialog>
+            {dialog}
         </div>
     )
 };
